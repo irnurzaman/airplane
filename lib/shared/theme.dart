e4120ce7a2c86404a01b95fd9a61f7e5911c07af
Ikhwan Rizqy Nurzaman <ikhwanrnurzaman@gmail.com>
2022-01-29 11:29:06 +0700

    chore: prepare color and font theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color theme
+Color purple = const Color(0xff5C40CC);
+Color red = const Color(0xffEB70A5);
+Color green = const Color(0xff0EC3AE);
+Color black = const Color(0xff1F1449);
+Color lightGrey = const Color(0xffDBD7EC);
+Color grey = const Color(0xff9698A9);
+Color orange = const Color(0xffFFA235);
+Color white = Colors.white;
+
+// Font theme
+TextStyle blackText = GoogleFonts.poppins(color: black);
+TextStyle whiteText = GoogleFonts.poppins(color: white);
+TextStyle greyText = GoogleFonts.poppins(color: grey);
+TextStyle greenText = GoogleFonts.poppins(color: green);
+TextStyle redText = GoogleFonts.poppins(color: red);
+TextStyle purpleText = GoogleFonts.poppins(color: purple);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
