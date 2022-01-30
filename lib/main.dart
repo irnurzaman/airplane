aacba4c1888cff6c96fabcd628af4f3a62670cc8
Ikhwan Rizqy Nurzaman <ikhwanrnurzaman@gmail.com>
2022-01-30 11:18:04 +0700

    feat: Add navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bwa_airplane/ui/pages/get_started.dart';
+import 'package:bwa_airplane/ui/pages/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Airplane',
-      home: GetStarted(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStarted()
+      },
     );
   }
 }
